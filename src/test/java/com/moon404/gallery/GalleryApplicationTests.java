7f599a06756efe04d3cd31bf7fbb28dd113e6a4e
Nguyễn Vân Phụng <phungnvwork@gmail.com>
2025-04-10 06:40:40 +0700

    update config

--- src/test/java/com/moon404/gallery/GalleryApplicationTests.java
+++ src/test/java/com/moon404/gallery/GalleryApplicationTests.java
@@ -3,11 +3,28 @@ package com.moon404.gallery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootTest
 class GalleryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() throws SQLException {
+
+//        public class TestConnection {
+//            public static void main(String[] args) throws Exception {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://db.wqqflvsxyicalmwrzqyx.supabase.co:5432/postgres",
+                "postgres",
+                "Moon@404"
+        );
+        System.out.println("✅ Connected!");
+        conn.close();
+//            }
+//        }
+
+    }
 
 }
